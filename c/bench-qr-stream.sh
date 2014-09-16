9c6b376c6d130c314900e561d9707e588af62ae8
Zongheng Yang <zongheng.y@gmail.com>
2014-09-15 23:19:22 -0700

    Updates.

--- c/bench-qr-stream.sh
+++ c/bench-qr-stream.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 stream_array_size=60000000
+batch_trials=4
+trial_sleep_time=120 # 2 minutes
 tsqr_rows="32768"
 tsqr_cols="512"
 
@@ -8,6 +10,7 @@ physical_cores=$((`cat /proc/cpuinfo | grep -i processor | wc -l` / 2))
 
 # turn off THP
 if [[ -e /sys/kernel/mm/transparent_hugepage/enabled ]]; then
+  echo "Turning off THP."
   echo never > /sys/kernel/mm/transparent_hugepage/enabled
   sleep 5
 fi
@@ -15,16 +18,37 @@ fi
 # make once
 make
 
-# run stream once
-wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
-gcc -O -fopenmp -D_OPENMP  -DSTREAM_ARRAY_SIZE=$stream_array_size stream.c -o stream 
-OMP_NUM_THREADS=1 ./stream >stream-`date "+%Y%m%d-%H:%M:%S"`.log
+# compile stream once
+if [ ! -f ./stream.c ]; then
+  echo "Downloading and compiling STREAM."
+  wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
+  gcc -O -fopenmp -D_OPENMP  -DSTREAM_ARRAY_SIZE=$stream_array_size stream.c -o stream
+fi
 
-# now run dgeqrf
-for rows in $tsqr_rows
+batch=0
+while true
 do
-  for cols in $tsqr_cols
+  batch=$(($batch+1))
+  if [[ $batch = "10" ]]; then
+    exit 0
+  fi
+
+  for i in `seq 1 $batch_trials`
   do
-    bash ./run-c-qr.sh $rows $cols $physical_cores >c-qr-`date "+%Y%m%d-%H:%M:%S"`.log
+    echo "Running STREAM."
+    # run stream
+    OMP_NUM_THREADS=$physical_cores ./stream >stream-batch$batch-`date "+%Y%m%d-%H:%M:%S"`.log | tee
+
+    # now run dgeqrf
+    for rows in $tsqr_rows
+    do
+      for cols in $tsqr_cols
+      do
+          echo "Running c-qr with $rows rows, $cols cols for each of the $physical_cores parallel threads."
+          bash ./run-c-qr.sh $rows $cols $physical_cores >c-qr-batch$batch-`date "+%Y%m%d-%H:%M:%S"`.log
+      done
+    done
   done
+  
+  sleep $trial_sleep_time
 done
